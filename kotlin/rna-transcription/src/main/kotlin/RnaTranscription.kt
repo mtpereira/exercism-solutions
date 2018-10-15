e860af42e090c70bfd95bcf06b02e6e8c2216b2c
Manuel Tiago Pereira <mt.pereira@gmail.com>
2018-10-15 23:42:57 +0100

    Add solution for kotlin rna-transcription.

---
+++ kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
@@ -0,0 +1,11 @@
+fun transcribeToRna(dna: String): String {
+    return dna.map {
+        when (it) {
+            'G' -> 'C'
+            'C' -> 'G'
+            'T' -> 'A'
+            'A' -> 'U'
+            else -> it
+        }
+    }.joinToString(separator = "")
+}
